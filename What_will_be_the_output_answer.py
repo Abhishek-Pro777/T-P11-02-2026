1af2fb115405bcb5a88103472d1276baea71178e
Abhishek-Pro777 <projectabhishek00@gmail.com>
2026-02-11 20:44:44 +0530

    What will be the output Answer

---
+++ What_will_be_the_output_answer.py
@@ -0,0 +1,31 @@
+x = 5
+x = x + 2
+x = x * 3
+print(x) # Output: 21
+
+a = 10
+b = 3
+print(a // b) # Output: 3
+print(a % b) # Output: 1
+
+x = 5
+y = 10
+print(x > y) # Output: False
+print(x < y and y > 5) # Output: True
+
+a = 4
+b = 2
+c = a ** b + b * 3
+print(c) # Output: 22
+
+x = 10
+x += 5
+x *= 2
+print(x) # Output: 30
+
+age = input("Enter age")
+print(age + 5) # This will cause an error because input returns a string
+
+a = 5
+b = "5"
+print(a + b) # This will cause an error because you cannot add an integer and a string
