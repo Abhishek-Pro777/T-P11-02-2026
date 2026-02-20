ebf6d25ad34bd85e2132279f5e4ef2a4678383b5
Abhishek-Pro777 <projectabhishek00@gmail.com>
2026-02-20 11:01:03 +0530

    Polymorphism Payment Gateway

---
+++ Poly_Payment.py
@@ -0,0 +1,22 @@
+class PaymentMethod:
+    def pay(self,amount):
+        raise NotImplementedError('Subclasses must implement this method')
+
+class CreditCardPayment(PaymentMethod):
+    def pay(self, amount):
+        return f'Processing credit card payment of Rs.{amount}'
+    
+class PaypalPayment(PaymentMethod):
+    def pay(self, amount):
+        return f'Processing credit card payment of Rs.{amount}'
+
+class CryptoPayment(PaymentMethod):
+    def pay(self, amount):
+        return f'Processing credit card payment of Rs.{amount}'
+    
+def process_payment(payment_method,amount):
+    print(payment_method.pay(amount))
+
+payment=[CreditCardPayment(), PaypalPayment(), CryptoPayment()]
+for method in payment:
+    process_payment(method,500)
